7590e3810a199d1647722e433f06f2fc17fa4d0c
siti raudha <siti@abgymsport.com>
2013-03-22 12:32:43 +1000

    updated files with existing errors

--- src/com/task/CreateWorkspace.java
+++ src/com/task/CreateWorkspace.java
@@ -19,15 +19,46 @@ import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View.OnClickListener;
-
+import android.location.Address;
 
 public class CreateWorkspace extends Activity{
 	
+	
+
+	private static final int REQUEST_CHOOSE_ADDRESS = 0;
 	private EditText name;
 	private Button create;
 	private DB db;
 	private int userID;
 	private String email;
+	private Address address;
+	private Button addLocationButton;
+	private TextView addressText;
+	
+	
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		if (null == address) { //check if there is an address
+			addLocationButton.setVisibility(View.VISIBLE);
+			addressText.setVisibility(View.GONE);
+		} else {
+			addLocationButton.setVisibility(View.GONE);
+			addressText.setVisibility(View.VISIBLE);
+			addressText.setText(address.getAddressLine(0));
+		}
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (REQUEST_CHOOSE_ADDRESS == requestCode && RESULT_OK == resultCode) {
+			address = data.getParcelableExtra(AddLocationMapActivity.ADDRESS_RESULT);
+		}else{
+		super.onActivityResult(requestCode, resultCode, data);
+	  }
+	}
+	
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.create_workspace);
@@ -38,6 +69,9 @@ public class CreateWorkspace extends Activity{
 		
 		name = (EditText)findViewById(R.id.workspaceName);
 		create = (Button)findViewById(R.id.create);
+		
+		addLocationButton = (Button)findViewById(R.id.add_location_button);
+		addressText = (TextView)findViewById(R.id.address_text);
 		
 		create.setOnClickListener(new OnClickListener(){
 
@@ -63,6 +97,11 @@ public class CreateWorkspace extends Activity{
 			
 		});
 		
+	}
+		
+		public void addLocationButtonClicked(View v){
+		Intent intent = new Intent(this, AddLocationMapActivity.class);
+		startActivityForResult(intent, REQUEST_CHOOSE_ADDRESS);
 	}
 	
 }
